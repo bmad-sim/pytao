edb549f1e3bd4c12a62e221c05d2d806a493cbc0
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-04-07 10:34:51 -0700

    CI: First attempt into automated tests.

---
+++ .github/scripts/install_bmad.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+mkdir -p ~/bmad
+pushd ~/bmad
+
+echo "**** Download and Extract Tarball"
+
+# Download Bmad
+curl -O https://www.classe.cornell.edu/~cesrulib/downloads/tarballs/${BMADDIST}.tgz \
+  && tar -xzf /$BMADDIST.tgz \
+  && mv $BMADDIST bmad_dist \
+  && rm -rf /$BMADDIST.tgz
+
+cd bmad_dist
+
+echo "**** Setup Preferences"
+
+cat <<EOF >> ./util/dist_prefs
+export DIST_F90_REQUEST="gfortran"
+export ACC_PLOT_PACKAGE="pgplot"
+export ACC_PLOT_DISPLAY_TYPE="X"
+export ACC_ENABLE_OPENMP="N"
+export ACC_ENABLE_MPI="N"
+export ACC_FORCE_BUILTIN_MPI="N"
+export ACC_ENABLE_GFORTRAN_OPTIMIZATION="Y"
+export ACC_ENABLE_SHARED="Y"
+export ACC_ENABLE_FPIC="Y"
+export ACC_ENABLE_PROFILING="N"
+export ACC_SET_GMAKE_JOBS="2"
+EOF
+
+echo "**** Invoking dist_source_me"
+source ./util/dist_source_me
+
+echo "**** Invoking dist_build_production"
+./util/dist_build_production
+
+popd
