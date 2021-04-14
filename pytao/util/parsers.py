1425e2af3579717f72ed78e19a32e4b7b9ccd011
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-04-14 16:24:57 -0700

    Add parse_tao_derivative

---
+++ pytao/util/parsers.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def parse_tao_derivative(lines):
+    """
+    Parses the output of tao python derivative
+    
+    Parameters
+    ----------
+    lines : list of str
+        The output of the 'python derivative' command to parse
+    
+    Returns
+    -------
+    out : dict
+        Dictionary with keys corresponding to universe indexes (int),
+        with dModel_dVar as the value:
+            np.ndarray with shape (n_data, n_var)    
+    """
+    universes = {}
+
+    # Build up matrices
+    for line in lines:
+        x = line.split(';')
+        iu = int(x[0])
+          
+        if iu not in universes:
+            # new universe
+            rows = universes[iu] = []
+            rowdat = []
+            row_id = int(x[1])
+        
+        if int(x[1]) == row_id:
+            # accumulate more data
+            rowdat += x[3:]     
+        else:
+            # Finish row
+            rows.append(rowdat)   
+            rowdat = x[3:]  
+            row_id = int(x[1])
+           
+    # cast to float    
+    out = {}
+    for iu, vals in universes.items():
+        out[iu] = np.array(vals).astype(float)
+        
+    return out
+
