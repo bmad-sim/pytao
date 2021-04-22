1261e3a273dd57cd47fed9ed72fcb9b0e94e6168
hhslepicka <hhslepicka@users.noreply.github.com>
2021-04-22 00:23:53 +0000

    deploy: 875804d65de9f62b3796697003749d933de4611b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0+7.g6fcd3ed',
+    VERSION: '0.1.0+9.g875804d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
