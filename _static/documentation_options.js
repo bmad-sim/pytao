94345c93923b1374d8a3f2f30e76787b15eff9d4
ChristopherMayes <ChristopherMayes@users.noreply.github.com>
2022-01-23 01:13:53 +0000

    deploy: 8c3ce6e9c94859773214ed3df0b18fb8945da99a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.6+10.gd28b24a',
+    VERSION: '0.1.6+12.g8c3ce6e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
