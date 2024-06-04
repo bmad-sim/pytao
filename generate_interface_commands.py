7f7afc87320b690f3c769b7467cfef92e95739aa
Christopher M. Pierce <contact@chris-pierce.com>
2024-06-04 10:21:31 -0700

    move docstring

--- generate_interface_commands.py
+++ generate_interface_commands.py
@@ -148,6 +148,31 @@ from pytao.util import parsers as __parsers
 
 
 def __execute(tao, cmd, as_dict=True, raises=True, method_name=None, cmd_type="string_list"):
+    \"""
+    
+    A wrapper to handle commonly used options when running a command through tao.
+
+    Parameters
+    ----------
+    tao : Tao
+      The handle to tao to run the command on
+    cmd : str
+      The command to run
+    as_dict : bool, optional
+      Return string data as a dict? by default True
+    raises : bool, optional
+      Raise exception on tao errors? by default True
+    method_name : str/None, optional
+      Name of the caller. Required for custom parsers for commands, by default None
+    cmd_type : str, optional
+      The type of data returned by tao in its common memory, by default "string_list"
+
+    Returns
+    -------
+    Any
+      Result from running tao. The type of data depends on configuration, but is generally a list of strings, a dict, or a
+      numpy array.
+    \"""
     func_for_type = {
         "string_list": tao.cmd,
         "real_array": tao.cmd_real,
