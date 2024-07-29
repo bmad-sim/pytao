ae3948632ebbe8db4b4783119f18afc4d998c4aa
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2024-07-29 10:59:16 -0700

    TST: run all examples with bokeh

--- pytao/tests/conftest.py
+++ pytao/tests/conftest.py
@@ -49,11 +49,47 @@ init_files = list(
 )
 
 
-@pytest.fixture(params=init_files, ids=[fn.name for fn in init_files])
+example_init_files = list(
+    path
+    for path in pathlib.Path(os.path.expandvars("$ACC_ROOT_DIR/bmad-doc/tao_examples")).glob(
+        "*/tao.init"
+    )
+)
+
+
+@pytest.fixture(params=init_files, ids=[f"regression_tests-{fn.name}" for fn in init_files])
 def init_filename(
     request: pytest.FixtureRequest,
 ) -> pathlib.Path:
     return request.param
+
+
+@pytest.fixture(params=example_init_files, ids=[fn.parts[-2] for fn in example_init_files])
+def example_init_filename(
+    request: pytest.FixtureRequest,
+) -> pathlib.Path:
+    example_name = request.param.parts[-2]
+    if example_name == "multi_turn_orbit":
+        pytest.skip(
+            "Multi-turn orbit example fails with: CANNOT SCALE GRAPH multi_turn.x SINCE NO DATA IS WITHIN THE GRAPH X-AXIS RANGE. "
+        )
+    if example_name == "custom_tao_with_measured_data":
+        # Looks to require some additional compilation and such
+        pytest.skip(
+            "'custom tao with measured data' example fails with PARSER ERROR DETECTED FOR UNIVERSE: 1"
+        )
+    if example_name == "x_axis_param_plot":
+        pytest.skip("'x_axis_param_plot' example fails saying no data is in range")
+    return request.param
+
+
+@pytest.fixture
+def example_nostartup(example_init_filename: pathlib.Path) -> bool:
+    return example_init_filename.parts[-1] in {
+        # "multi_turn_orbit",
+        "custom_tao_with_measured_data",
+        "x_axis_param_plot",
+    }
 
 
 @pytest.fixture(
@@ -65,7 +101,13 @@ def tao_cls(request: pytest.FixtureRequest):
 
 
 @contextlib.contextmanager
-def new_tao(tao_cls, init, plot: bool = False, external_plotting: bool = True, **kwargs):
+def new_tao(
+    tao_cls,
+    init,
+    plot: bool = False,
+    external_plotting: bool = True,
+    **kwargs,
+):
     init = os.path.expandvars(init)
     if external_plotting:
         init = " ".join((init, "-external_plotting"))
--- pytao/tests/test_plot_bokeh.py
+++ pytao/tests/test_plot_bokeh.py
@@ -5,22 +5,64 @@ import re
 import pytest
 from bokeh.plotting import column, output_file, save
 
-from .. import Tao
+from pytao.subproc import SubprocessTao
+
 from ..plotting.bokeh import BGraphAndFigure, share_common_x_axes
 from .conftest import new_tao, test_artifacts
 
 logger = logging.getLogger(__name__)
 
 
-def test_bokeh_manager(request: pytest.FixtureRequest, init_filename: pathlib.Path):
+def test_bokeh_manager(
+    request: pytest.FixtureRequest,
+    init_filename: pathlib.Path,
+):
     name = re.sub(r"[/\\]", "_", request.node.name)
     filename_base = f"bokeh_{name}"
-
-    # Floor orbit tries to set a plot setting on initialization which doesn't work
-    # with external plotting.
-    nostartup = init_filename.name == "tao.init_floor_orbit"
-    with new_tao(Tao, f"-init {init_filename}", nostartup=nostartup) as tao:
+    with new_tao(SubprocessTao, init="", init_file=init_filename) as tao:
         manager = tao.bokeh
+        assert len(manager.place_all())
+
+        output_file(test_artifacts / f"{filename_base}.html")
+
+        bgraphs = sum(manager.plot_regions(list(manager.regions)), [])
+        items = [
+            BGraphAndFigure(bgraph=bgraph, fig=bgraph.create_figure()) for bgraph in bgraphs
+        ]
+        for item in items:
+            fig = item.fig
+            graph = item.bgraph.graph
+            fig.title.text = f"{fig.title.text} ({graph.region_name}.{graph.graph_name} of {request.node.name})"
+
+        share_common_x_axes(items)
+        save(column([item.fig for item in items], sizing_mode="fixed"))
+
+        for region in list(manager.regions):
+            manager.clear(region)
+        assert not any(region for region in manager.regions.values())
+        manager.clear()
+        assert not manager.regions
+
+
+def test_bokeh_examples(
+    request: pytest.FixtureRequest,
+    example_init_filename: pathlib.Path,
+    example_nostartup: bool,
+):
+    example_name = example_init_filename.parts[-2]
+    name = re.sub(r"[/\\]", "_", request.node.name)
+    filename_base = f"bokeh_{name}"
+    with new_tao(
+        SubprocessTao,
+        init="",
+        init_file=example_init_filename,
+        nostartup=example_nostartup,
+    ) as tao:
+        manager = tao.bokeh
+
+        if example_name == "erl":
+            tao.cmd("place r11 zphase")
+
         assert len(manager.place_all())
 
         output_file(test_artifacts / f"{filename_base}.html")
