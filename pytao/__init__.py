76ebcd166eace5bfa111e78c86dcd944892577af
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2024-08-29 09:12:20 -0700

    API: export exceptions to top-level pytao.*

--- pytao/__init__.py
+++ pytao/__init__.py
@@ -9,6 +9,11 @@ in the util package.
 from .interface_commands import Tao, TaoStartup
 from .subproc import AnyTao, SubprocessTao
 from .tao_ctypes import TaoModel, run_tao
+from .tao_ctypes.core import (
+    TaoException,
+    TaoInitializationError,
+    TaoSharedLibraryNotFoundError,
+)
 from .tao_ctypes.evaluate import evaluate_tao
 from .tao_interface import tao_interface
 from .tao_pexpect import tao_io
@@ -22,7 +27,10 @@ __all__ = [
     "AnyTao",
     "SubprocessTao",
     "Tao",
+    "TaoException",
+    "TaoInitializationError",
     "TaoModel",
+    "TaoSharedLibraryNotFoundError",
     "TaoStartup",
     "evaluate_tao",
     "run_tao",
