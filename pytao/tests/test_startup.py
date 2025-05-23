1b682011fbc5dd5e56f24a0c835f8c687fc9f932
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2025-05-23 16:52:22 -0700

    ENH: add Tao.from_lattice_contents

--- pytao/tests/test_startup.py
+++ pytao/tests/test_startup.py
@@ -75,3 +75,56 @@ def test_startup_bad_file() -> None:
         assert bad_fn in ex.tao_output
         assert "TAO INITIALIZATION FILE NOT FOUND" in ex.tao_output
         print(str(ex))
+
+
+fodo_lattice = """
+no_digested
+parameter[geometry] = closed
+parameter[particle] = electron
+
+beginning[e_tot] = 150e6
+beginning[x_position]     = 1e-3
+beginning[y_position]     = 1e-3
+beginning[z_position]     = 0.
+
+L_tot = 2
+Lq = 0.1
+Ld = (L_tot - 2*Lq)/2
+
+k1_optimal =  (1/Lq) * 2*sqrt(2) / L_tot
+
+K1: kicker, L=0.1
+
+P0: pipe
+
+P1: P0, L = Ld
+
+q0: quad, L = Lq
+q1: q0
+q2: q0
+
+O_L: overlay = {p1[L]:(Lcell - 2*Lq)/2}, var = {Lcell}
+O_L[Lcell] = L_tot
+
+q1[k1] = k1_optimal
+q2[k1] = -k1_optimal
+
+fodo1: line = (k1, p1, q1, p1, q2 )
+
+use, fodo1
+"""
+
+
+def test_startup_from_lattice_contents() -> None:
+    tao = Tao.from_lattice_contents(fodo_lattice)
+    names = [tao.ele_head(ele_id)["name"] for ele_id in tao.unique_ele_ids()]
+    assert names == [
+        "BEGINNING",
+        "K1",
+        "O_L",
+        "P1",
+        "Q1",
+        "P1",
+        "Q2",
+        "END",
+    ]
