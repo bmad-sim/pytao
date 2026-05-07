9c51cf599fce01a7afa336f7ddc1f89808099589
Christopher M. Pierce <contact@chris-pierce.com>
2026-05-07 11:32:54 -0700

    add to config

--- pytao/unittest/config.py
+++ pytao/unittest/config.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
 
+from pytao.unittest.tests import unit_test_types
+
 
 class LatticeConfig(BaseModel):
     lattice_file: str | None = None
@@ -7,4 +9,5 @@ class LatticeConfig(BaseModel):
 
 
 class UnittestConfig(BaseModel):
-    lattices: dict[str, LatticeConfig] = Field(default_factory=list, description="Mapping from unique lattice identifier to lattice loading information")
+    lattices: dict[str, LatticeConfig] = Field(default_factory=dict, description="Mapping from unique lattice identifier to lattice loading information")
+    tests: list[unit_test_types] = Field(default_factory=list, description="Unit tests to run against the lattices")
