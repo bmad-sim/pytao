5855aa005b99cdfa082a297ad46f8ea117419b9f
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-09-15 11:56:45 -0500

    MAINT: use get_element_index

--- pytao/constraints/observables/ele.py
+++ pytao/constraints/observables/ele.py
@@ -6,8 +6,6 @@ from typing import Literal
 import numpy as np
 from pydantic import Field, computed_field
 
-from pytao.constraints.pydantic import ConstraintsBase
-
 from pytao import Tao
 from pytao.constraints.observables.base import (
     CheckResult,
@@ -20,6 +18,7 @@ from pytao.constraints.observables.base import (
     Observation,
 )
 from pytao.constraints.observables.twiss import AnyTwissComparison, BmagTwissComparison
+from pytao.constraints.pydantic import ConstraintsBase
 from pytao.model import (
     ElementFloor,
     ElementFloorAll,
@@ -28,7 +27,7 @@ from pytao.model import (
     ElementTwiss,
 )
 from pytao.model import _generated as tao_classes
-from pytao.model.ele.ele import Element
+from pytao.model.ele.ele import Element, get_element_index
 
 
 class EleObservation(Observation):
@@ -633,17 +632,14 @@ def _ele_reduce(
     ix_begin: int | None = None
     ix_end: int | None = None
 
-    def get_ix_ele(ele: str | int) -> int:
-        return tao_classes.ElementHead.from_tao(tao, ele_id=ele, which="model").ix_ele
-
     if begin_ele is not None or end_ele is not None:
-        ix_end_marker = get_ix_ele("END")
+        ix_end_marker = get_element_index(tao, "END")
         if begin_ele is not None:
-            ix_begin = get_ix_ele(begin_ele)
+            ix_begin = get_element_index(tao, begin_ele)
             if ix_begin >= ix_end_marker:
                 raise ValueError(f"begin_ele {begin_ele!r} is not a tracking element")
         if end_ele is not None:
-            ix_end = get_ix_ele(end_ele)
+            ix_end = get_element_index(tao, end_ele)
             if ix_end >= ix_end_marker:
                 raise ValueError(f"end_ele {end_ele!r} is not a tracking element")
 
